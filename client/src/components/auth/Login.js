a54e7d53fd75b02945d1490e5b88e5115c65ad62
Mitul David <mituldavid@gmail.com>
2020-06-13 08:43:54 +0530

    Redux Setup and Alerts

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export const Login = () => {
+const Login = () => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -52,3 +52,4 @@ export const Login = () => {
     </Fragment>
   );
 };
+export default Login;
