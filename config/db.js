cdf7111dbfc96a3f7d7e6dad1f60c34a5bb9ea85
Mitul David <mituldavid@gmail.com>
2020-06-15 17:34:03 +0530

    Prep for Deployment

--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
+      useFindAndModify: true,
     });
     console.log('Connected to MongoDB');
   } catch (err) {
